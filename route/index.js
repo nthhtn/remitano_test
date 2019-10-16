9e3dfddba056cfb2d2ac5b814173a3fd1c6803c7
nthhtn <thehien115@gmail.com>
2019-10-16 15:22:56 +0300

    Login

--- route/index.js
+++ route/index.js
@@ -1,20 +1,39 @@
 import express from 'express';
 import { isLoggedIn, isNotLoggedIn } from '../lib/auth';
 import UserModel from '../model/user';
+import VideoModel from '../model/video';
+import { generateRandomString, hashPassword } from '../lib/password';
+import to from 'await-to-js';
 
 module.exports = (app, db) => {
 
 	const router = express.Router();
 	const User = new UserModel(db);
+	const Video = new VideoModel(db);
 
 	router.route('/login')
 		.post(isNotLoggedIn, async (req, res) => {
-			
+			const { email, password } = req.body;
+			let [err, result] = await to(User.readByEmail(email));
+			if (err) { return res.status(500).json({ success: false, error: err.message }); }
+			if (result) {
+				const hashed = hashPassword(password, result.salt);
+				if (hashed === result.password) {
+					req.session.user = email;
+					return res.json({ success: true });
+				}
+				return res.status(401).json({ success: false, error: 'Wrong password' });
+			}
+			const salt = generateRandomString();
+			const hashedPassword = hashPassword(password, salt);
+			[err, result] = await to(User.create({ email, salt, password: hashedPassword }));
+			if (err) { return res.status(500).json({ success: false, error: err.message }); }
+			return res.json({ success: true });
 		});
 
 	router.route('/')
 		.get(async (req, res) => {
-			return res.render('base', { user: req.session.user || null });
+			return res.render('base', { user: req.session.user || undefined });
 		});
 
 	app.use(router);
--- static/js/guest.js
+++ static/js/guest.js
@@ -1,15 +1,29 @@
+function validateEmail(email) {
+	let pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return pattern.test(email);
+};
+
 $('#login-btn').on('click', () => {
 	let email = $('#login-email').val();
+	if (!validateEmail(email)) {
+		$('#login-form .login-error').html('Invalid email');
+		return;
+	}
 	let password = $('#login-password').val();
+	if (!password) {
+		$('#login-form .login-error').html('Invalid password');
+		return;
+	}
 	$.ajax({
 		url: '/login',
 		method: 'POST',
 		data: { email: email, password: password },
 		success: (response) => {
-
+			window.location.href = '/';
 		},
 		error: (error) => {
-
+			console.log(error);
+			$('#login-form .login-error').html('Wrong password');
 		}
 	});
 });
